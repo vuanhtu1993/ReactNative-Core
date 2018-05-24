9aa79a2e29058120323774e60abf77dd421c145e
vuanhtu1993mta@gmail.com <tuntun159753>
2018-05-25 01:44:49 +0700

    adding clock img

--- components/Clock.js
+++ components/Clock.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import {Button, Text, TextInput, View} from "react-native";
+import React, {Component} from 'react';
+import {Button, Text, TextInput, View, StyleSheet, Image} from "react-native";
 
 export default class Clock extends Component {
   constructor() {
@@ -9,6 +9,7 @@ export default class Clock extends Component {
       inputTime: '',
     }
   }
+
   handleInput = (text) => {
     this.setState({
       inputTime: text,
@@ -27,6 +28,7 @@ export default class Clock extends Component {
       });
     }, 1000)
   };
+
   render() {
     let duration = this.state.duration;
     return (
@@ -42,6 +44,7 @@ export default class Clock extends Component {
           title="Count down"
         />
         <ClockDisplay duration={duration}/>
+        <ClockReal/>
       </View>
     );
   }
@@ -57,3 +60,49 @@ const ClockDisplay = ({duration}) => {
     </View>
   );
 };
+
+const ClockReal = () => {
+  return (
+    <View style={styles.clock}>
+      <Image style={{height: 200, width: 200}}
+             source={{uri: 'https://i.pinimg.com/originals/70/1f/dd/701fdd0228fd4e9ef9877be8dabcb5ca.png'}}/>
+      <Image style={styles.sec} source={{uri: 'https://css-tricks.com/examples/CSS3Clock/images/sechand.png'}}/>
+      <Image style={styles.min} source={{uri: 'https://css-tricks.com/examples/CSS3Clock/images/minhand.png'}}/>
+      <Image style={styles.hour} source={{uri: 'https://css-tricks.com/examples/CSS3Clock/images/hourhand.png'}}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    clock: {
+      position: 'relative',
+      width: 600,
+      height: 600,
+      margin: 20,
+    },
+    sec: {
+      position: "absolute",
+      width: 10,
+      height: 100,
+      top: 30,
+      left: 285,
+      zIndex: 3,
+    },
+    min: {
+      position: "absolute",
+      width: 10,
+      height: 100,
+      top: 30,
+      left: 285,
+      zIndex: 2,
+    },
+    hour: {
+      position: "absolute",
+      width: 10,
+      height: 100,
+      top: 30,
+      left: 285,
+      zIndex: 1,
+    }
+  })
+;

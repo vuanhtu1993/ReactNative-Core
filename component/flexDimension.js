62357dab62d88deefabc3cefd0558249c3dd3773
vuanhtu1993mta@gmail.com <tuntun159753>
2018-05-25 13:55:17 +0700

    layout with flex

---
+++ component/flexDimension.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {AppRegistry, StyleSheet, Text, TextInput, View, Alert, Button, TouchableHighlight} from 'react-native';
+
+export default class FlexDimension extends Component{
+  render() {
+    return (
+      <View style={styles.flexParent}>
+        <View style={{ height: 100, width: 100, backgroundColor: 'powderblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'skyblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'steelblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'powderblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'skyblue' }}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  flexParent: {
+    flex: 1,
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  }
+});

b8599123e81f0dfb065321bd1fa19c4f791bb082
vuanhtu1993mta@gmail.com <tuntun159753>
2018-05-25 17:05:39 +0700

    Keyboard module demo and add keyboard native event to componentDidMount

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {AppRegistry, StyleSheet, Text, TextInput, View, Alert, Button, TouchableHighlight} from 'react-native';
 import FlexDimension from "./component/flexDimension";
+import TextInputDemo from "./component/textInput";
 
 export default class App extends Component {
   render() {
     return (
-      <FlexDimension/>
+      <TextInputDemo/>
     );
   }
 }
--- component/flexDimension.js
+++ component/flexDimension.js
@@ -1,49 +1,26 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry, StyleSheet, Text, TextInput, View, Alert, Button, TouchableHighlight,
-  TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, ScrollView
-} from 'react-native';
+import {AppRegistry, StyleSheet, Text, TextInput, View, Alert, Button, TouchableHighlight} from 'react-native';
 
 export default class FlexDimension extends Component{
-  constructor() {
-    super();
-    this.state = {
-      inputText: "",
-    }
-  }
   render() {
     return (
-      <ScrollView>
-        <View style={styles.container}>
-          <View style={{ height: 200, width: 200, backgroundColor: 'powderblue' }}/>
-          <View style={{ height: 200, width: 200, backgroundColor: 'gray' }}/>
-          <View style={{ height: 200, width: 200, backgroundColor: 'orange' }}/>
-          <View style={{ height: 200, width: 200, backgroundColor: 'pink' }}/>
-        </View>
-      </ScrollView>
+      <View style={styles.flexParent}>
+        <View style={{ height: 100, width: 100, backgroundColor: 'powderblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'skyblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'steelblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'powderblue' }}/>
+        <View style={{ height: 100, width: 100, backgroundColor: 'skyblue' }}/>
+      </View>
     );
   }
 }
 
-const Box = () => {
-  return (
-    <View style={{ height: 200, width: 200, backgroundColor: 'powderblue' }}/>
-  );
-};
-
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    alignItems: 'center'
-  },
-  button: {
-    marginBottom: 30,
-    width: 260,
+  flexParent: {
+    flex: 1,
+    flexDirection: 'column',
+    flexWrap: 'nowrap',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#2196F3'
-  },
-  buttonText: {
-    padding: 20,
-    color: 'white'
   }
 });

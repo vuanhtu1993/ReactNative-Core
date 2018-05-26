cac1e4a242e0998d0df9e7f1f68b7c32234cf89f
vuanhtu1993mta@gmail.com <tuntun159753>
2018-05-26 15:45:26 +0700

    button demo

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import {AppRegistry, StyleSheet, Text, TextInput, View, Alert, Button, TouchableHighlight} from 'react-native';
 import FlexDimension from "./component/flexDimension";
 import TextInputDemo from "./component/textInput";
+import ButtonDemo from "./component/button";
+import ListViewDemo from "./component/ListView";
 
 export default class App extends Component {
   render() {
     return (
-      <TextInputDemo/>
+      <ListViewDemo/>
     );
   }
 }

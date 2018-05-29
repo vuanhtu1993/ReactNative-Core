1052a8b43d9f47f9adc8ff4fcde96a9b38f39833
vuanhtu1993mta@gmail.com <tuntun159753>
2018-05-29 23:45:12 +0700

    create layout with ScrollView (horizontal, default vertical)

--- App.js
+++ App.js
@@ -4,11 +4,12 @@ import FlexDimension from "./component/flexDimension";
 import TextInputDemo from "./component/textInput";
 import ButtonDemo from "./component/button";
 import ListViewDemo from "./component/ListView";
+import ScrollViewDemo from "./component/scrollview";
 
 export default class App extends Component {
   render() {
     return (
-      <FlexDimension/>
+      <ScrollViewDemo/>
     );
   }
 }

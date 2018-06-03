6575735bac2427b9e49061ebb006f34fcb2007ff
vuanhtu1993mta@gmail.com <tuntun159753>
2018-06-03 23:39:23 +0700

    update

--- component/todos.js
+++ component/todos.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {FlatList, Text, View, StyleSheet} from "react-native";
+import {FlatList, Text, View, StyleSheet, TextInput} from "react-native";
 
 export default class TodoApp extends Component {
   constructor() {
@@ -9,23 +9,35 @@ export default class TodoApp extends Component {
       todos: [{ id: 1, name: "monday", done: false }, { id: 2, name: "tuesday", done: false }],
     }
   }
-  renderItem = ({item}) => {
-    return (
-      <Text style={styles.row}>
-        {item.text}
-      </Text>
-    )
-  };
+  handleSubmitEditing(text) {
+    this.setState({
+      inputText: text
+    })
+  }
   render() {
-    const extractKey = ({id}) => id;
+    console.log(this.state);
     return (
       <View style={styles.container}>
         <Text>Todo application</Text>
+        <TextInput
+          placeholder='Adding todos'
+          style={{
+            margin: 10,
+            height: 50,
+            padding: 10,
+            borderColor: 'gray',
+            borderWidth: 1,
+            borderRadius: 5,
+            color: 'gray',
+          }}
+          blurOnSubmit={true}
+          onSubmitEditing={this.handleSubmitEditing}
+          onChangeText={(text) => this.setState({inputText: text})}
+        />
         <FlatList
           style = {styles.container}
           data = {this.state.todos}
           renderItem={({item}) => <Text style={styles.row}>{item.name}</Text>}
-          keyExtractor={extractKey}
         />
       </View>
     );
